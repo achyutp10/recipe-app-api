97c574093ae0b020581d96f88bbdc3fff67b50a4
achyut <achyutp10@gmail.com>
2024-06-04 18:07:53 +0545

    configured docker compose and checks to wait for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
 
     }
